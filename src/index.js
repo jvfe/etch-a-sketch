7cf0345645b3b5e313fe74f5ef344be1c9517e82
jvfe <jvfecav@gmail.com>
2021-02-01 08:35:03 -0300

    feat: Add basic functionality for 16x16 grid

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const canvasGrid = document.querySelector('.canvas-grid');
+
+function makeGrid(dimension) {
+
+    canvasGrid.style.setProperty("--grid-cols", dimension);
+    canvasGrid.style.setProperty("--grid-rows", dimension);
+
+    for (let i = 0; i < dimension * dimension; i++) {
+        const square = document.createElement('div');
+        canvasGrid.appendChild(square);
+        square.classList.add('square');
+    }
+}
+
+let dimension = 16;
+
+makeGrid(16);
